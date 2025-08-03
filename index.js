9128dab64c86d9d6bcfe556d985f7e6d1fc1342c
Unknown 💀 <skiniyan44@gmail.com>
2025-08-03 20:21:16 +0530

    Initial commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Node.js CI/CD pipeline is working!");
+  res.send("Node.js CI/CD pipeline is working!...");
 });
 
 module.exports = app;
